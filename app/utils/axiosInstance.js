900e3aef707fec222c67dbf11ed847e682148206
Tanjim Alam <122804862+tanjim-alam@users.noreply.github.com>
2025-03-19 23:51:06 +0530

    Update axiosInstance.js

--- app/utils/axiosInstance.js
+++ app/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8082/api/v1";
+// const BASE_URL = "http://localhost:8082/api/v1";
 
-// const BASE_URL = "https://searchmyspacebackend-production.up.railway.app/api/v1";
+const BASE_URL = "https://searchmyspacebackend-production.up.railway.app/api/v1";
 
 const axiosInstance = axios.create();
 axiosInstance.defaults.baseURL = BASE_URL;
